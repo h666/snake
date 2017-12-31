1c2de62c3ed3c6590986a6ee1dabcb0d4d59d997
h666 <zerodigital@outlook.com>
2017-12-31 10:15:30 +0000

    Updated script to include auto movement

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ var vdown=0;
 var vright=0;
 var vleft=0;
 
+var playerDirection=0; //player direction for automovement of player
+
 var keys=[];
 var width=500,height=400,speed=5;
 
@@ -22,8 +24,10 @@ var player={   //player character for control
 };
 
 var cube={ //enemy!
-	x:Math.random()*(width-20),
-	y:Math.random()*(height-20),
+	//x:Math.random()*(width-20),
+	//y:Math.random()*(height-20),
+	y:Math.floor(Math.random() * 10) * 40,
+	x:Math.floor(Math.random() * 10) * 40,
 	width:20,
 	height:20
 };
@@ -44,35 +48,41 @@ right-39
 
 function game(){
 	update();
-	render();
+	//render();
+	movement();
+	collision();
 }
 
 function update(){
 	//if(keys[38]) console.log("up"); good for reporting, i will write this into an array myself to check input history(20)
 	if(keys[38]){
-		player.y-=speed;
+		//player.y-=speed;
 		//console.log("Up");
 		con2.clearRect(0,0,49,12);
 		con2.fillText("Up " + vup++,10,10);
+		playerDirection=1;
 	}
 	if(keys[40]){
-		player.y+=speed;
+		//player.y+=speed;
 		//console.log("Down");
 		con2.clearRect(50,0,49,12);
 		con2.fillText("Down " + vdown++,50,10);
+		playerDirection=2;
 	}
 	if(keys[37]){
-		player.x-=speed;
+		//player.x-=speed;
 		//console.log("Left");
 		con2.clearRect(105,0,49,12);
 		con2.fillText("Left " + vleft++,105,10);
+		playerDirection=3;
 	}
 
 	if(keys[39]){
-		player.x+=speed;
+		//player.x+=speed;
 		//console.log("Right");
 		con2.clearRect(155,0,49,12);
 		con2.fillText("Right " + vright++,155,10);
+		playerDirection=4;
 	}
 	
 	if(player.x<0)player.x=0;
@@ -82,12 +92,58 @@ function update(){
 	
 }
 
+/*
 function render(){
 	con1.clearRect(0,0,width,height);
 	con1.fillStyle="Blue";
 	con1.fillRect(player.x, player.y, player.width, player.height);
 	con1.fillStyle="Yellow";
 	con1.fillRect(cube.x, cube.y, cube.width, cube.height);
+}
+*/
+
+function movement(){
+	switch(playerDirection){
+		case 1:
+			con1.clearRect(0,0,width,height);
+			con1.fillStyle="Blue";
+			con1.fillRect(player.x, player.y-=speed, player.width, player.height);
+			con1.fillStyle="Yellow";
+			con1.fillRect(cube.x, cube.y, cube.width, cube.height);
+		break;
+
+		case 2:
+			con1.clearRect(0,0,width,height);
+			con1.fillStyle="Blue";
+			con1.fillRect(player.x, player.y+=speed, player.width, player.height);
+			con1.fillStyle="Yellow";
+			con1.fillRect(cube.x, cube.y, cube.width, cube.height);
+		break;
+
+		case 3:
+			con1.clearRect(0,0,width,height);
+			con1.fillStyle="Blue";
+			con1.fillRect(player.x-=speed, player.y, player.width, player.height);
+			con1.fillStyle="Yellow";
+			con1.fillRect(cube.x, cube.y, cube.width, cube.height);
+		break;
+
+		case 4:
+			con1.clearRect(0,0,width,height);
+			con1.fillStyle="Blue";
+			con1.fillRect(player.x+=speed, player.y, player.width, player.height);
+			con1.fillStyle="Yellow";
+			con1.fillRect(cube.x, cube.y, cube.width, cube.height);
+		break;
+	}
+}
+
+function collision(){
+	//collision detection
+	if(player.x==Math.round(cube.x)&&player.y==Math.round(cube.y)){
+		alert("done!");
+	}
+	console.log(player.x,player.y,cube.x,cube.y);
 }
 
 
